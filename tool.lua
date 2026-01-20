d6a9edfc62a14e5607ec4333fc8407106b396f17
guilhhotina <lottiecake@proton.me>
2026-01-20 20:24:44 -0300

    feat(tool): translated tool to english

--- tool.lua
+++ tool.lua
@@ -23,46 +23,44 @@ local CMD = is_win and {
     cd    = "(cd %s && %s)"
 }
 
--- helper para executar comandos
+-- run cmd
 local function run(fmt, ...)
     local cmd = string.format(fmt, ...)
     print(">> " .. cmd)
     local ok = os.execute(cmd)
     if not ok then
-        print("[ERRO] Falha no comando."); os.exit(1)
+        print("[erro] cmd failed."); os.exit(1)
     end
 end
 
--- garante q a engine existe
+-- check if engine exists, download if not
 local function setup_engine()
     if not io.open(PATH.engine .. "/README.md") then
-        print("[INFO] Baixando engine...")
+        print("[info] downloading engine...")
         run(CMD.mkdir, "vendor")
         run("git clone --depth 1 %s %s", PATH.engine_url, PATH.engine)
     end
 end
 
--- isso cpia o jogo para dentro da engine (fix de path do LÖVE)
+-- copy game to engine folder (fix path issue with love)
 local function sync_game()
     local dest = PATH.engine .. "/" .. PATH.game_src
     if is_win then run(CMD.mkdir, dest:gsub("/", "\\")) else run(CMD.mkdir, dest) end
     run(CMD.cp, PATH.game_src, dest)
 end
 
--- isso executa CLI da Engine
+-- run engine cli
 local function engine_cli(args)
     setup_engine()
     sync_game()
-    -- isso aq executa o comando de dentro da pasta da engine
     run(CMD.cd, PATH.engine, "lua source/cli/main.lua " .. args)
 end
 
--- tarefas
+-- tasks
 if task == "dev" then
     engine_cli("run src/game.lua")
 elseif task == "build" then
     run(CMD.rm, "dist")
-    -- o output acho q tem que ser relativo à pasta da engine (../../dist/web)
     engine_cli("build-html src/game.lua --outdir ../../" .. PATH.dist .. " --enginecdn")
 elseif task == "clean" then
     run(CMD.rm, "vendor")
@@ -71,5 +69,5 @@ elseif task == "deploy" then
     os.execute("lua tool.lua build")
     os.execute("fly deploy")
 else
-    print("Uso: lua tool.lua [dev | build | clean | deploy]")
+    print("uso: lua tool.lua [dev | build | clean | deploy]")
 end
